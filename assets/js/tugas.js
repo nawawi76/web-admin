562d55575c5bffb01489c95356a86763531b9337
nawawi76 <147130520+nawawi76@users.noreply.github.com>
2023-12-20 20:51:35 -0800

    km

--- assets/js/tugas.js
+++ assets/js/tugas.js
@@ -1,24 +1,24 @@
 function validasiSubmit() {
     // Mendapatkan waktu saat ini
-    var currentTime = new Date();
-    var hours = currentTime.getHours();
+    let currentTime = new Date();
+    let hours = currentTime.getHours();
 
     // Validasi waktu submit
     if (hours <= 10) {
-        document.getElementById("output").innerHTML = "Anda Keren";
+        alert("Anda Keren");
     } else if (hours > 10 && hours <= 11) {
-        document.getElementById("output").innerHTML = "Poin Anda minus 10";
+        alert("Poin Anda minus 10");
         
         // Cek alasan submit
-        var alasan = prompt("Mengapa Anda submit setelah pukul 10?");
+        let alasan = prompt("Mengapa Anda submit setelah pukul 10?");
 
         if (alasan.toLowerCase() === "lupa") {
-            document.getElementById("output").innerHTML += "<br>Nilai 0";
+            alert("<br>Nilai 0");
         } else {
-            document.getElementById("output").innerHTML += "<br>Nilai akan dikurangi 30";
+            alert("<br>Nilai akan dikurangi 30");
         }
     } else {
-        document.getElementById("output").innerHTML = "Anda tidak mengerjakan tugas";
+        alert("Anda tidak mengerjakan tugas");
     }
 
     // Menampilkan waktu submit
